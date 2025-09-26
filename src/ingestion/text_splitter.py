0f6f043f45744313acc078c1045e4ee524ade52e
grol <getahune.alemayhu@gmail.com>
2025-09-26 12:07:50 +0300

    Spit documents into smaller chunks

--- src/ingestion/text_splitter.py
+++ src/ingestion/text_splitter.py
@@ -1,6 +1,7 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def split_documents(documents, chunks_size=500, chunk_overlap=50);
+# Function to split documents into smaller chunks
+def split_documents(documents, chunks_size=500, chunk_overlap=50):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunks_size,
         chunk_overlap=chunk_overlap,
