386df4a7a68bad29924f86abb6f72e4f291e240f
Getahun Alemayhu <getahune.alemayhu@gmail.com>
2025-09-29 22:14:19 +0300

    Build chain based on promp templates

--- src/llm/chain.py
+++ src/llm/chain.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 from src.llm.prompt_templates import get_prompt_template
 from src.retrieval.retriever import get_retriever
 
-def build_chain(query_type: str):
+def build_chain(query_type: str, chain_type: str= "stuff"):
     retriever = get_retriever()
     prompt_template = get_prompt_template(query_type)
     llm = ChatGroq(
@@ -14,9 +14,12 @@ def build_chain(query_type: str):
 )
     chain = retrieval_qa.RetrievalQA.from_chain_type(
         llm=llm,
-        chain_type="stuff",
+        chain_type=chain_type,
         retriever=retriever,
         return_source_documents=True,
         chain_type_kwargs={"prompt": prompt_template}
     )
+    
+    print(f"🔗 Chain built with model: {llm.model}, prompt type: {query_type}")
+      
     return chain
